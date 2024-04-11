48a4be6b6e45e78075ddd5a5094efe8e7190c5b1
ayat-liberatelabs <ayat@liberate-labs.com>
2024-04-12 00:07:18 +0600

    team create score and toss drawer added

--- src/app/match/[id]/page.tsx
+++ src/app/match/[id]/page.tsx
@@ -1,5 +1,60 @@
-import React from "react";
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import React, { useEffect, useState } from "react";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Toss } from "@/components/forms/toss";
 
-export default function MatchAvsB() {
-  return <div>MatchAvsB</div>;
+export default function MatchAvsB({ params }: { params: { id: string } }) {
+  const [isTossCompleted, setIsTossCompleted] = useState(false);
+  useEffect(() => {}, []);
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Card className="w-[400px] pt-5 mx-5 md:mx-0">
+        <div className=" flex justify-center items-center space-x-10  ">
+          <div className="flex justify-center items-center flex-col space-y-2">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h1>Team 1</h1>
+          </div>
+          <div className="flex justify-center items-center flex-col space-y-2">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h1>Team 2</h1>
+          </div>
+        </div>
+        <div className="flex flex-col space-y-1 pt-2">
+          <h1 className="text-center font-bold">Over: 5</h1>
+          <h1 className="text-center  font-bold">Wickets: 5</h1>
+        </div>
+        <CardFooter>
+          <div className="flex justify-center w-full py-5">
+            {isTossCompleted ? (
+              <Button
+                onClick={() => {
+                  setIsTossCompleted(false);
+                }}
+              >
+                Do Scoring
+              </Button>
+            ) : (
+              <Toss />
+            )}
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
---
+++ src/components/forms/toss.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { set, useForm } from "react-hook-form";
+import { createTeam } from "@/lib/actions/team.action";
+
+const createTeamFormSchema = z.object({
+  teamName: z
+    .string()
+    .min(2, {
+      message: "Team name should be at least 2 characters long",
+    })
+    .max(50, {
+      message: "Team name should not exceed 50 characters",
+    }),
+  teamColor: z
+    .string()
+    .min(3, {
+      message: "Team name should be at least 3 characters long",
+    })
+    .max(20, {
+      message: "Team name should not exceed 20 characters",
+    }),
+});
+
+export function Toss() {
+  const [open, setOpen] = useState(false);
+  const form = useForm<z.infer<typeof createTeamFormSchema>>({
+    resolver: zodResolver(createTeamFormSchema),
+    defaultValues: {
+      teamName: "",
+      teamColor: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof createTeamFormSchema>) {
+    const res = await createTeam(values);
+
+    if (res.success) {
+      alert("Team created successfully");
+      form.reset();
+      setOpen(false);
+    } else {
+      console.log(res.error);
+      alert("Failed to create team");
+    }
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="outline">Toss</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle className="text-center">Create Your Team</DrawerTitle>
+          <DrawerDescription className="text-center">
+            Fill up the form here. Click save when you are done.
+          </DrawerDescription>
+        </DrawerHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 md:w-1/2 md:mx-auto mx-3"
+          >
+            <FormField
+              control={form.control}
+              name="teamName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Team Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter Your Team Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="teamColor"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Team Color</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter Your Team Color" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+              <Button type="submit" className="w-full sm:w-auto">
+                Create Team
+              </Button>
+              <DrawerClose asChild>
+                <Button variant="outline" className="w-full sm:w-auto">
+                  Cancel
+                </Button>
+              </DrawerClose>
+            </div>
+          </form>
+        </Form>
+        <DrawerFooter className="pt-2"></DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
